7e7b091bff96306950dcd550ceb98ea1e3c9b16d
Petr Dvorak <petrdvor@gmail.com>
2015-11-17 15:04:43 +0100

    Fixed with default in if

--- themeSwitcher.m
+++ themeSwitcher.m
@@ -2,7 +2,10 @@ function [ out ] = themeSwitcher( in,color )
 %THEMESWITCHER Summary of this function goes here
 %   Detailed explanation goes here
 
+if color
 out = theme(in,color);
-
+else
+    out = theme(in,'default');
+end
 end
 
