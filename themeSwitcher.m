e0af34290e508fc444e70a3b2ce594642c401cd5
Petr Dvorak <petrdvor@gmail.com>
2015-11-17 15:02:09 +0100

    Code polishing

--- themeSwitcher.m
+++ themeSwitcher.m
@@ -2,6 +2,7 @@ function [ out ] = themeSwitcher( in )
 %THEMESWITCHER Summary of this function goes here
 %   Detailed explanation goes here
 
-% out = theme(in,'white');
+out = theme(in,'white');
+
 end
 
