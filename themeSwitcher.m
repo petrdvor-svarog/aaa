da23b0091549fd54504fabfe31f151aabd917f83
Petr Dvorak <petrdvor@gmail.com>
2015-11-17 15:03:01 +0100

    General color switching

--- themeSwitcher.m
+++ themeSwitcher.m
@@ -1,8 +1,8 @@
-function [ out ] = themeSwitcher( in )
+function [ out ] = themeSwitcher( in,color )
 %THEMESWITCHER Summary of this function goes here
 %   Detailed explanation goes here
 
-out = theme(in,'white');
+out = theme(in,color);
 
 end
 
