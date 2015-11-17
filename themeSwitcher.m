d69579c4cd1b3f6bc1fe3edf226a3be8cfa8679b
Petr Dvorak <petrdvor@gmail.com>
2015-11-17 14:08:38 +0100

    Updated to white theme

--- themeSwitcher.m
+++ themeSwitcher.m
@@ -2,6 +2,6 @@ function [ out ] = themeSwitcher( in )
 %THEMESWITCHER Summary of this function goes here
 %   Detailed explanation goes here
 
-% out = theme(in,'black');
+% out = theme(in,'white');
 end
 
